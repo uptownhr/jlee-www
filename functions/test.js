8df5d40e2a347feb2c3a1a10c31944bbb6ba520d
James Lee <uptownhr@gmail.com>
2020-06-16 00:06:05 -0700

    another one

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,7 @@
 exports.handler = function(event, context, callback) {
   // your server-side functionality
   const body = JSON.stringify({
-    event, context, test: process.env
+    event, context, test: process.env, stripe: process.env.STRIPE_PRIVATE_KEY
   })
   callback(null, {
     statusCode: 200,
