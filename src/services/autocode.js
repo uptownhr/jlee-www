c9e06d060134b2c8561cd8e5cbc1b553be885e60
James Lee <uptownhr@gmail.com>
2021-04-08 17:12:23 -0700

    should work in prod

--- src/services/autocode.js
+++ src/services/autocode.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+let devTag = process.env.AUTOCODE_ENVIRONMENT ? `@${process.env.AUTOCODE_ENVIRONMENT}` : ''
+
 const client = axios.create({
-  baseURL: `https://uptownhr.api.stdlib.com/jlee@${process.env.AUTOCODE_ENVIRONMENT}`
+  baseURL: `https://uptownhr.api.stdlib.com/jlee${devTag}`
 })
 
 export default client
