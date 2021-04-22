2395acbc9edc4005ce0f5d67cd606d476e6744ba
James Lee <uptownhr@gmail.com>
2021-04-22 07:55:13 -0700

    added responsive alignment

--- src/components/PageCTABreak/PageCTABreak.vue
+++ src/components/PageCTABreak/PageCTABreak.vue
@@ -4,7 +4,7 @@
         class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between"
     >
       <h2
-          class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
+          class="sm:text-center text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
       >
         <slot name="heading-text"></slot>
       </h2>
