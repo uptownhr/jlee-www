8058a9a926f5dd089dae151b387590172ee4e72c
James Lee <uptownhr@gmail.com>
2021-05-05 17:25:21 -0700

    using a more relevant image

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -48,8 +48,8 @@
     >
       <img
         class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-        alt=""
+        src="/img/james-self-backside.jpeg"
+        alt="James coding from the back"
       />
     </div>
   </div>
