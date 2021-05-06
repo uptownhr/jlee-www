6f80b27ec007eaec377fc1be4691da746af1d526
uptown <uptownhr@gmail.com>
2021-05-06 07:21:35 -0700

    Hiding portrait in mobile

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="relative bg-white overflow-hidden">
+  <div class="relative overflow-hidden">
     <div class="max-w-screen-xl mx-auto">
       <div
-        class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+        class="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
       >
-        <svg
-          class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-          fill="currentColor"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="none"
-        >
-          <polygon points="50,0 100,0 50,100 0,100" />
-        </svg>
-
         <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
           <Nav />
         </div>
@@ -44,7 +35,7 @@
     </div>
     <hr class="md:hidden" />
     <div
-      class="hidden md:block md:show lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
+      class="hidden lg:block md:show lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
     >
       <img
         class="h-56 object-cover sm:h-72 md:h-96 lg:h-full"
